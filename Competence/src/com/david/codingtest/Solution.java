f8f34952dd666813a79a7da5aaa05849d8d73bc7
DavidJeonKr <dudgh7410@naver.com>
2021-12-01 22:45:28 +0900

    .

---
+++ Competence/src/com/david/codingtest/Solution.java
@@ -0,0 +1,21 @@
+package com.david.codingtest;
+
+public class Solution {
+	public int[] solution(int[] lottos, int[] win_nums) {
+		int[] answer = {};
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		answer[0] = 1;
+		answer[1] = 2;
+		
+		
+		return answer;
+	}
+}
